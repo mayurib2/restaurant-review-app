716c922e5570626a7af741b9a64432d53f948bec
Mayuri Bhise <bhisemayuri@gmail.com>
2019-08-10 15:17:49 -0700

    Feat: Add accessibility features

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
-  initMap(); // added 
+  initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibWF5dXJpLWIiLCJhIjoiY2p5ejBpMHJ5MDFuZjNpbnhwYWd2djIyciJ9.hm7Dk34AfSj3jQjRz9Uu9w',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -165,6 +165,8 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+  // Adding alt tag for accessibility
+  image.alt = `${restaurant.name} 's image`;
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -178,6 +180,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabindex = 0;
   li.append(more)
 
   return li
@@ -194,10 +197,16 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     function onClick() {
       window.location.href = marker.options.url;
     }
+
+    // Accessibility feature for keyboard users
+    marker.on("keypress", keyPress);
+    function keyPress() {
+      window.location.href = marker.options.url;
+    }
     self.markers.push(marker);
   });
 
-} 
+}
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
@@ -208,4 +217,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
-
